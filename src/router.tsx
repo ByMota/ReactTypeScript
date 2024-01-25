cb8a45abaafc3dabd032ed8cd7206990dbf98a95
Vinicius Mota <vinicius.motamorici@gmail.com>
2024-01-24 21:04:39 -0300

    useReducer

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ import {UseRef} from "./pages/useRef";
 import {CustomHook} from "./pages/customHook";
 import Fetch from "./pages/Fetch";
 import Context from "./pages/Context";
+import Form from "./pages/Form";
 
 export function AppRouter() {
   return(
@@ -18,6 +19,7 @@ export function AppRouter() {
         <Route path="/custom-hook" element={<CustomHook/>}/>
         <Route path="/fetch" element={<Fetch/>}/>
         <Route path="/context" element={<Context/>}/>
+        <Route path="/form" element={<Form/>} />
       </Routes>
     </Router>
   )
